6f662a9fefb9a94e6a10266c6cf9f3e34cb2d083
root <root@localhost>
2024-02-08 19:28:04 +0000

    adaptive equipment added to UserApplication table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_08_163556) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_08_192347) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,6 +69,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_08_163556) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.text "adaptive_equipment"
   end
 
   create_table "users", force: :cascade do |t|
