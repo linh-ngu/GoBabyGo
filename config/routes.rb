77d41973266c98239e3c6d24c2cc4035892b3c84
Linh Nguyen <linh.n565@gmail.com>
2024-02-08 21:23:59 -0600

    configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :user_applications do
+    member do
+      get :delete
+    end
+  end
   get 'main/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "main#index"
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
