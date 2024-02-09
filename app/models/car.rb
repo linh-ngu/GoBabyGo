c4fa1f1f97af1a29ec4d59f77ea72941608eb687
root <root@localhost>
2024-02-09 19:17:35 +0000

    fixed car and part relationship

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,5 @@
 class Car < ApplicationRecord
-    has_many :parts
+    has_and_belongs_to_many :parts
     belongs_to :car_type
     belongs_to :finance
     belongs_to :user_application, optional: true
