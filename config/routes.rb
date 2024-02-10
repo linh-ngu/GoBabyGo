1da4bf43abbf53ec122299d23e95495cbd666449
ethanortiz25 <ethan@loszitros.com>
2024-02-10 11:16:48 -0600

    car and car_type basic crud

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'main/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "main#index"
+  resources :cars
+  resources :car_types
   # Defines the root path route ("/")
   # root "articles#index"
 end
