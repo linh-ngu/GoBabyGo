93b6ea22e2853464812e9518d906bdd29fada8eb
ethanortiz25 <ethan@loszitros.com>
2024-02-11 16:09:32 -0600

    passing rainy day test

--- spec/feature/car_integration_spec.rb
+++ spec/feature/car_integration_spec.rb
@@ -5,13 +5,22 @@ RSpec.describe 'Creating a car', type: :feature do
   scenario 'valid inputs' do
     visit new_car_path
     fill_in 'Modification details', with: 'Test modification details'
-    puts "Available car type options:"
-    page.all('select#car_type_id option').each do |option|
-      puts option.text
-    end
     select 'SUV', from: 'Car Type'
     check 'Complete?'
     click_on 'Create Car'
     expect(page).to have_text('car was successfully created.')
+  end
+  scenario 'invalid inputs' do
+    # Visit the new car page
+    visit new_car_path
+    
+    # Fill in the modification details and check the complete checkbox
+    fill_in 'Modification details', with: 'Test modification details AAA'
+    check 'Complete?'
+    
+    # Click on the 'Create Car' button
+    expect {
+      click_on 'Create Car'
+    }.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
