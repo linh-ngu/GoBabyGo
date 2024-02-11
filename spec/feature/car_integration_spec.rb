d0d0b257772c84c486bd0514c28984ff1e2528d5
ethanortiz25 <ethan@loszitros.com>
2024-02-11 14:38:06 -0600

    tweaking unit tests

--- spec/feature/car_integration_spec.rb
+++ spec/feature/car_integration_spec.rb
@@ -4,6 +4,9 @@ require 'rails_helper'
 RSpec.describe 'Creating a car', type: :feature do
   scenario 'valid inputs' do
     visit new_car_path
+    fill_in 'Modification details', with: 'Test modification details'
+    select 'SUV', from: 'Car Type'
+    check 'Complete?'
     click_on 'Create Car'
     expect(page).to have_current_path(cars_path)
     expect(page).to have_text('Car successfully created')
--- spec/feature/car_type_integration_spec.rb
+++ spec/feature/car_type_integration_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe 'Creating a car type', type: :feature do
   scenario 'valid inputs' do
     visit new_car_type_path
     fill_in 'Name', with: 'SUV'
+    fill_in 'Max height', with: 200
+    fill_in 'Min height', with: 150
+    fill_in 'Price', with: 25000
     click_on 'Create Car type'
     expect(page).to have_current_path(car_types_path)
     expect(page).to have_text('Car type successfully created')
