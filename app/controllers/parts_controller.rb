97a5b0d88bfdd34bcf94d769f1faa0569b4e93eb
ethanortiz25 <ethan@loszitros.com>
2024-02-12 14:21:44 -0600

    part deletion now on car screen

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -58,7 +58,7 @@ class PartsController < ApplicationController
       @part.destroy
   
       respond_to do |format|
-        format.html { redirect_to parts_url, notice: "part was successfully destroyed." }
+        # format.html { redirect_to parts_url, notice: "part was successfully destroyed." }
         format.json { head :no_content }
       end
     end
