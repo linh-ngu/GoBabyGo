7dd80191ce54ff71701e969cea73c8fc693fe4ed
Daniel Querrey <daniel.q@tamu.edu>
2024-02-12 22:26:29 -0600

    Changed sign in route back to dashboards#show and root route is back to main
    added a new route to call dashboards#show and changed the api keys

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
   root to: 'main#index'
 
   # root to: 'dashboards#show'
+  get 'dashboard', to: 'dashboards#show', as: 'dashboard'
+
   devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
   devise_scope :admin do
     get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
