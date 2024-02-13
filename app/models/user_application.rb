ab973eaef3dcc8260d78df6319c8c8fd41a144e9
root <root@localhost>
2024-02-13 04:32:51 +0000

    added user_id

--- app/models/user_application.rb
+++ app/models/user_application.rb
@@ -9,4 +9,6 @@ class UserApplication < ApplicationRecord
     validates :caregiver_name, presence: true
     validates :caregiver_email, presence: true
     validates :caregiver_phone, presence: true
+    validates :can_transport, inclusion: { in: [true, false] }
+    validates :can_store, inclusion: { in: [true, false] }
 end
