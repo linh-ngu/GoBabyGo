5b3371735b11bdfc1e524b6808dd452b033038ee
Daniel Querrey <daniel.q@tamu.edu>
2024-02-20 11:38:11 -0600

    Enum values added to application record

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
     has_many :user_applications
     belongs_to :admin, optional: true
+
+    enum level: {
+        applicant: 1,
+        staff_member: 2,
+        officer_member: 3,
+        admin: 4
+    }
+
 end
