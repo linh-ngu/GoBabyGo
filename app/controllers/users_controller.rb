4633379ea08d6a3c9b05bb7d50190979cbd97d2c
Linh Nguyen <linh.n565@gmail.com>
2024-02-20 19:42:25 -0600

    edit user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find_by(admin_id: current_admin.id)
+  end
+
+  def update
+    @user = User.find_by(admin_id: current_admin.id)
+    if @user.update(user_params)
+      redirect_to dashboard_path
+    else
+      render('edit')
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(
