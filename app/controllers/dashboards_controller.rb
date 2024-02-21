9fe3755f115a5b6233fc73109838dcf0d230ce13
Daniel Querrey <daniel.q@tamu.edu>
2024-02-20 21:34:43 -0600

    Created 3 user table views & began controllerparams

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,6 +4,7 @@ class DashboardsController < ApplicationController
       redirect_to new_user_path
     else
       @user = User.find_by(admin_id: current_admin.id)
+      @users = User.where(level: User.levels[:visitor])
 
       if @user.applicant?
         render 'show_user'
@@ -16,5 +17,26 @@ class DashboardsController < ApplicationController
       end
     end
   end
+
+  def show_table_users
+    if current_admin.user_account_created == false
+      redirect_to new_user_path
+    else
+      @user = User.find_by(admin_id: current_admin.id)
+      @user_level = @user.level if @user.present?
+      @users = User.where(level: User.levels[:visitor])
+
+      if @user.applicant?
+        render 'show_user'
+      elsif @user.officer_member?
+        render 'show_officer'
+      elsif @user.admin?
+        render 'show_admin'
+      else
+        render 'show_user'
+      end
+    end
+  end
+
 
 end
