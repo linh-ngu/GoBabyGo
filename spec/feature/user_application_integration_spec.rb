061b564546ed1decd2a72219e17b54fd7b1cbfe2
root <root@localhost>
2024-02-22 20:41:35 +0000

    integration test for edifor editing profile and making an application

--- spec/feature/user_application_integration_spec.rb
+++ spec/feature/user_application_integration_spec.rb
@@ -1,31 +1,33 @@
 # location: spec/features/user_application_integration_spec.rb
-#require 'rails_helper'
-
-=begin
-##################################################
-TODO: GET GOOGLE OAUTH INTEGRATION TESTING WORKING
-@Daniel Querrey
-##################################################
+require 'rails_helper'
 
 RSpec.describe 'APPLICANT: Creation of a user application', type: :feature do
+    include Devise::Test::IntegrationHelpers
+
+    before do
+        @admin = Admin.create!(email: 'test@gmail.com', full_name: 'Test Admin', uid: '123456', avatar_url: 'http://example.com/avatar')
+        sign_in @admin
+        @user = User.create!(email: 'test@gmail.com', phone: '1234567890', admin_id: @admin.id)
+    end
+
     scenario 'VALID: visit user application path, create application with all fields filled' do
         # login_with_oauth here
         visit user_applications_path
         click_on 'New Application'
         expect(page).to have_text('New Application')
-        fill_in 'Child\'s Name', with: 'John Doe'
-        fill_in 'Child\'s Birthdate', with: '2022-01-01'
-        fill_in 'Child\'s Primary Diagnosis', with: 'Primary Diagnosis Test'
-        fill_in 'Child\'s Secondary Diagnosis (if applicable)', with: 'Secondary Diagnosis Test'
-        fill_in 'What adaptive equipment does your child use?', with: 'Adaptive Equipment Test'
-        fill_in 'Child\'s Height', with: '100'
-        fill_in 'Child\'s Weight', with: '50'
-        fill_in 'Caregiver\'s Name', with: 'Jane Doe'
-        fill_in 'Caregiver\'s Email', with: 'jane@example.com'
-        fill_in 'Caregiver\'s Phone', with: '1234567890'
-        choose 'Yes' # Selects radio button for 'Do you have the ability to transport the car on Build Day?'
-        choose 'Yes' # Selects radio button for 'Do you have the ability to store the car?'
-        fill_in 'Additional Notes', with: 'Additional Notes Test'
+        fill_in 'user_application_child_name', with: 'John Doe'
+        fill_in 'user_application_child_birthdate', with: '2022-01-01'
+        fill_in 'user_application_primary_diagnosis', with: 'Primary Diagnosis Test'
+        fill_in 'user_application_secondary_diagnosis', with: 'Secondary Diagnosis Test'
+        fill_in 'user_application_adaptive_equipment', with: 'Adaptive Equipment Test'
+        fill_in 'user_application_child_height', with: '100'
+        fill_in 'user_application_child_weight', with: '50'
+        fill_in 'user_application_caregiver_name', with: 'Jane Doe'
+        fill_in 'user_application_caregiver_email', with: 'jane@example.com'
+        fill_in 'user_application_caregiver_phone', with: '1234567890'
+        choose 'user_application_can_transport_true' # Selects radio button for 'Do you have the ability to transport the car on Build Day?'
+        choose 'user_application_can_store_true' # Selects radio button for 'Do you have the ability to store the car?'
+        fill_in 'user_application_notes', with: 'Additional Notes Test'
         click_on 'Submit Application'
         expect(page).to have_content("Application submitted successfully. We will reach out to you soon with our response.")
         user_application = UserApplication.last
@@ -39,17 +41,16 @@ RSpec.describe 'APPLICANT: Creation of a user application', type: :feature do
         visit user_applications_path
         click_on 'New Application'
         expect(page).to have_text('New Application')
-        fill_in 'Child\'s Name', with: 'John Doe'
-        fill_in 'Child\'s Birthdate', with: '2022-01-01'
-        fill_in 'Child\'s Primary Diagnosis', with: 'Primary Diagnosis Test'
-        fill_in 'What adaptive equipment does your child use?', with: 'Adaptive Equipment Test'
-        fill_in 'Child\'s Height', with: '100'
-        fill_in 'Child\'s Weight', with: '50'
-        fill_in 'Caregiver\'s Name', with: 'Jane Doe'
-        fill_in 'Caregiver\'s Email', with: 'jane@example.com'
-        fill_in 'Caregiver\'s Phone', with: '123-456-7890'
-        choose 'Yes' # Selects radio button for 'Do you have the ability to transport the car on Build Day?'
-        choose 'Yes' # Selects radio button for 'Do you have the ability to store the car?'
+        fill_in 'user_application_child_name', with: 'John Doe'
+        fill_in 'user_application_child_birthdate', with: '2022-01-01'
+        fill_in 'user_application_primary_diagnosis', with: 'Primary Diagnosis Test'
+        fill_in 'user_application_child_height', with: '100'
+        fill_in 'user_application_child_weight', with: '50'
+        fill_in 'user_application_caregiver_name', with: 'Jane Doe'
+        fill_in 'user_application_caregiver_email', with: 'jane@example.com'
+        fill_in 'user_application_caregiver_phone', with: '1234567890'
+        choose 'user_application_can_transport_true' # Selects radio button for 'Do you have the ability to transport the car on Build Day?'
+        choose 'user_application_can_store_true' # Selects radio button for 'Do you have the ability to store the car?'
         click_on 'Submit Application'
         expect(page).to have_content("Application submitted successfully. We will reach out to you soon with our response.")
         user_application = UserApplication.last
@@ -63,55 +64,55 @@ RSpec.describe 'APPLICANT: Creation of a user application', type: :feature do
         visit user_applications_path
         click_on 'New Application'
         expect(page).to have_text('New Application')
-        visit user_applications_path
-        click_on 'New Application'
-        expect(page).to have_text('New Application')
-        fill_in 'Child\'s Name', with: 'John Doe'
-        fill_in 'Child\'s Birthdate', with: '2022-01-01'
-        fill_in 'Child\'s Primary Diagnosis', with: 'Primary Diagnosis Test'
-        fill_in 'What adaptive equipment does your child use?', with: 'Adaptive Equipment Test'
-        fill_in 'Child\'s Height', with: '100'
-        fill_in 'Child\'s Weight', with: '50'
-        fill_in 'Caregiver\'s Name', with: 'Jane Doe'
-        fill_in 'Caregiver\'s Email', with: 'jane@example.com'
-        choose 'Yes' # Selects radio button for 'Do you have the ability to transport the car on Build Day?'
-        choose 'Yes' # Selects radio button for 'Do you have the ability to store the car?'
-        click_on 'Submit Application'
+        fill_in 'user_application_child_name', with: 'John Doe'
+        fill_in 'user_application_child_birthdate', with: '2022-01-01'
+        fill_in 'user_application_primary_diagnosis', with: 'Primary Diagnosis Test'
+        fill_in 'user_application_child_height', with: '100'
+        fill_in 'user_application_child_weight', with: '50'
+        fill_in 'user_application_caregiver_name', with: 'Jane Doe'
+        fill_in 'user_application_caregiver_email', with: 'jane@example.com'
+        choose 'user_application_can_transport_true' # Selects radio button for 'Do you have the ability to transport the car on Build Day?'
+        choose 'user_application_can_store_true' # Selects radio button for 'Do you have the ability to store the car?'
         click_on 'Submit Application'
         expect(page).to have_content("Caregiver phone can't be blank")
     end
 
     scenario 'INVALID: visit user application path, create application with all fields empty' do
         # login_with_oauth here
-        visit user_applications_path
+        visit user_applications_path(@user)
         click_on 'New Application'
         expect(page).to have_text('New Application')
         click_on 'Submit Application'
         expect(page).to have_content("Child name can't be blank, Child birthdate can't be blank, Primary diagnosis can't be blank, Child height can't be blank, Child weight can't be blank, Caregiver name can't be blank, Caregiver email can't be blank, Caregiver phone can't be blank, Can transport is not included in the list, Can store is not included in the list")
     end
 end
-
-
 
 RSpec.describe 'OFFICER: Creation of a user application', type: :feature do
+    include Devise::Test::IntegrationHelpers
+    before do
+        @admin = Admin.create!(email: 'test@gmail.com', full_name: 'Test Admin', uid: '123456', avatar_url: 'http://example.com/avatar')
+        sign_in @admin
+        @user = User.create!(email: 'test@gmail.com', phone: '1234567890', admin_id: @admin.id, level: 3)
+    end
+    
     scenario 'VALID: visit user application path, create application with all fields filled' do
         # login_with_oauth here
         visit user_applications_path
         click_on 'New Application'
         expect(page).to have_text('New Application')
-        fill_in 'Child\'s Name', with: 'John Doe'
-        fill_in 'Child\'s Birthdate', with: '2022-01-01'
-        fill_in 'Child\'s Primary Diagnosis', with: 'Primary Diagnosis Test'
-        fill_in 'Child\'s Secondary Diagnosis (if applicable)', with: 'Secondary Diagnosis Test'
-        fill_in 'What adaptive equipment does your child use?', with: 'Adaptive Equipment Test'
-        fill_in 'Child\'s Height', with: '100'
-        fill_in 'Child\'s Weight', with: '50'
-        fill_in 'Caregiver\'s Name', with: 'Jane Doe'
-        fill_in 'Caregiver\'s Email', with: 'jane@example.com'
-        fill_in 'Caregiver\'s Phone', with: '1234567890'
-        choose 'Yes' # Selects radio button for 'Do you have the ability to transport the car on Build Day?'
-        choose 'Yes' # Selects radio button for 'Do you have the ability to store the car?'
-        fill_in 'Additional Notes', with: 'Additional Notes Test'
+        fill_in 'user_application_child_name', with: 'John Doe'
+        fill_in 'user_application_child_birthdate', with: '2022-01-01'
+        fill_in 'user_application_primary_diagnosis', with: 'Primary Diagnosis Test'
+        fill_in 'user_application_secondary_diagnosis', with: 'Secondary Diagnosis Test'
+        fill_in 'user_application_adaptive_equipment', with: 'Adaptive Equipment Test'
+        fill_in 'user_application_child_height', with: '100'
+        fill_in 'user_application_child_weight', with: '50'
+        fill_in 'user_application_caregiver_name', with: 'Jane Doe'
+        fill_in 'user_application_caregiver_email', with: 'jane@example.com'
+        fill_in 'user_application_caregiver_phone', with: '1234567890'
+        choose 'user_application_can_transport_true' # Selects radio button for 'Do you have the ability to transport the car on Build Day?'
+        choose 'user_application_can_store_true' # Selects radio button for 'Do you have the ability to store the car?'
+        fill_in 'user_application_notes', with: 'Additional Notes Test'
         click_on 'Submit Application'
         expect(page).to have_content("Application submitted successfully.")
         user_application = UserApplication.last
@@ -125,17 +126,16 @@ RSpec.describe 'OFFICER: Creation of a user application', type: :feature do
         visit user_applications_path
         click_on 'New Application'
         expect(page).to have_text('New Application')
-        fill_in 'Child\'s Name', with: 'John Doe'
-        fill_in 'Child\'s Birthdate', with: '2022-01-01'
-        fill_in 'Child\'s Primary Diagnosis', with: 'Primary Diagnosis Test'
-        fill_in 'What adaptive equipment does your child use?', with: 'Adaptive Equipment Test'
-        fill_in 'Child\'s Height', with: '100'
-        fill_in 'Child\'s Weight', with: '50'
-        fill_in 'Caregiver\'s Name', with: 'Jane Doe'
-        fill_in 'Caregiver\'s Email', with: 'jane@example.com'
-        fill_in 'Caregiver\'s Phone', with: '123-456-7890'
-        choose 'Yes' # Selects radio button for 'Do you have the ability to transport the car on Build Day?'
-        choose 'Yes' # Selects radio button for 'Do you have the ability to store the car?'
+        fill_in 'user_application_child_name', with: 'John Doe'
+        fill_in 'user_application_child_birthdate', with: '2022-01-01'
+        fill_in 'user_application_primary_diagnosis', with: 'Primary Diagnosis Test'
+        fill_in 'user_application_child_height', with: '100'
+        fill_in 'user_application_child_weight', with: '50'
+        fill_in 'user_application_caregiver_name', with: 'Jane Doe'
+        fill_in 'user_application_caregiver_email', with: 'jane@example.com'
+        fill_in 'user_application_caregiver_phone', with: '1234567890'
+        choose 'user_application_can_transport_true' # Selects radio button for 'Do you have the ability to transport the car on Build Day?'
+        choose 'user_application_can_store_true' # Selects radio button for 'Do you have the ability to store the car?'
         click_on 'Submit Application'
         expect(page).to have_content("Application submitted successfully.")
         user_application = UserApplication.last
@@ -149,21 +149,15 @@ RSpec.describe 'OFFICER: Creation of a user application', type: :feature do
         visit user_applications_path
         click_on 'New Application'
         expect(page).to have_text('New Application')
-        visit user_applications_path
-        click_on 'New Application'
-        expect(page).to have_text('New Application')
-        fill_in 'Child\'s Name', with: 'John Doe'
-        fill_in 'Child\'s Birthdate', with: '2022-01-01'
-        fill_in 'Child\'s Primary Diagnosis', with: 'Primary Diagnosis Test'
-        fill_in 'What adaptive equipment does your child use?', with: 'Adaptive Equipment Test'
-        fill_in 'Child\'s Height', with: '100'
-        fill_in 'Child\'s Weight', with: '50'
-        fill_in 'Caregiver\'s Name', with: 'Jane Doe'
-        fill_in 'Caregiver\'s Email', with: 'jane@example.com'
-        choose 'Yes' # Selects radio button for 'Do you have the ability to transport the car on Build Day?'
-        choose 'Yes' # Selects radio button for 'Do you have the ability to store the car?'
-
-        click_on 'Submit Application'
+        fill_in 'user_application_child_name', with: 'John Doe'
+        fill_in 'user_application_child_birthdate', with: '2022-01-01'
+        fill_in 'user_application_primary_diagnosis', with: 'Primary Diagnosis Test'
+        fill_in 'user_application_child_height', with: '100'
+        fill_in 'user_application_child_weight', with: '50'
+        fill_in 'user_application_caregiver_name', with: 'Jane Doe'
+        fill_in 'user_application_caregiver_email', with: 'jane@example.com'
+        choose 'user_application_can_transport_true' # Selects radio button for 'Do you have the ability to transport the car on Build Day?'
+        choose 'user_application_can_store_true' # Selects radio button for 'Do you have the ability to store the car?'
         click_on 'Submit Application'
         expect(page).to have_content("Caregiver phone can't be blank")
     end
@@ -177,4 +171,3 @@ RSpec.describe 'OFFICER: Creation of a user application', type: :feature do
         expect(page).to have_content("Child name can't be blank, Child birthdate can't be blank, Primary diagnosis can't be blank, Child height can't be blank, Child weight can't be blank, Caregiver name can't be blank, Caregiver email can't be blank, Caregiver phone can't be blank, Can transport is not included in the list, Can store is not included in the list")
     end
 end
-=end
